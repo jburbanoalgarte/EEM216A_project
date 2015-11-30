3fa52347b3478baabf9984475070505659978edc
Jordi Burbano <jburbanoalgarte@gmail.com>
2015-11-29 23:04:07 -0800

    housekeeping

--- EE216A_F15_FinalProject_v2/verilog/define.h
+++
@@ -1,8 +0,0 @@
-`define activehigh 1
-`define procline posedge clk
-`define proclineg posedge clk
-`define proclineSlow posedge clkSlow
-`define proclineFast posedge clkFast
-`define proclinerclk posedge rclk
-`define proclinewclk posedge wclk
-`define grsttype "synch"
